67d248095e5361c7fb4a70cb956ffeb762dc4ecd
stanreyRee <yuhongli2@outlook.com>
2017-02-28 17:22:36 +0800

    Added EditText for username

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     private int numOfCoffee = 0;
-    private int priceOfCoffee = 2;
-    private int priceOfWhippedCream = 1;
+
+    // prices
+    private float coffeePrice = 2.00f;
+    private float whippedCreamPrice = 0.50f;
+    private float chocolatePrice = 0.50f;
+
     private boolean hasWhippedCream = false;
+    private boolean hasChocolate = false;
+
+    // name
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int finalPrice = this.calculatePrice();
+        this.getUserName();
+        float finalPrice = this.calculatePrice();
         this.displayPrice(finalPrice);
     }
 
@@ -50,39 +60,56 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the reset button is clicked.
      */
     public void reset(View view) {
+        // reset userName
+        this.resetUserName();
+
+        // reset Coffee
         resetNumOfCoffee();
         displayQuantity();
-        displayPrice(-1);
+        displayPrice(-1.0f);
+
+        // reset the checkboxes
+        this.resetChocolateCheckBox();
+        this.resetWhippedCreamCheckBox();
+
     }
 
     /**
      * This method calculates the final price on the screen.
      */
-    private int calculatePrice() {
+    private float calculatePrice() {
         // check if whippedCream is checked;
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         this.hasWhippedCream = whippedCreamCheckBox.isChecked();
-        int finalSinglePrice = this.priceOfCoffee
-                + (this.hasWhippedCream ? this.priceOfWhippedCream : 0);
+
+        // check if chocolate is checked;
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        this.hasChocolate = chocolateCheckBox.isChecked();
+
+        float finalSinglePrice = this.coffeePrice
+                + (this.hasWhippedCream ? this.whippedCreamPrice : 0)
+                + (this.hasChocolate ? this.chocolatePrice : 0);
         return finalSinglePrice * this.numOfCoffee;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayPrice(int rawPrice) {
+    private void displayPrice(float rawPrice) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
 
-        // This info will show welcome info after reset button is clicked
-        if (rawPrice == -1) {
+        // This info will show initial price information when total price is 0.
+        if (Float.compare(rawPrice, -1.0f) == 0 || Float.compare(rawPrice, 0.0f) == 0) {
             priceTextView.setText("$0");
             return;
         }
 
         String TotalPrice = NumberFormat.getCurrencyInstance().format(rawPrice);
 //        Log.v("MainActicity","The price is: " + priceInfo);
-        String info = "Quantity: " + this.numOfCoffee;
+        String info = "Name: " + this.userName;
+        info += "\nQuantity: " + this.numOfCoffee;
         info += "\nAdd whipped cream: " + this.hasWhippedCream;
+        info += "\nAdd chocolate: " + this.hasChocolate;
         info += "\nTotal: " + TotalPrice;
         info += "\nThank you!";
         priceTextView.setText(info);
@@ -117,5 +144,43 @@ public class MainActivity extends AppCompatActivity {
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
+    }
+
+
+    /**
+     * This method resets whippedCreamCheckBox.
+     */
+    private void resetWhippedCreamCheckBox() {
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        if (this.hasWhippedCream) {
+            whippedCreamCheckBox.toggle();
+        }
+    }
+
+    /**
+     * This method resets whippedCreamCheckBox.
+     */
+    private void resetChocolateCheckBox() {
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        if (this.hasChocolate) {
+            chocolateCheckBox.toggle();
+        }
+    }
+
+
+    /**
+     * This method gets user input of name and store it.
+     */
+    private void getUserName() {
+        EditText userNameInput = (EditText) findViewById(R.id.name_field);
+        this.userName = userNameInput.getText().toString();
+    }
+
+    /**
+     * This method resets name field to null.
+     */
+    private void resetUserName() {
+        EditText userNameInput = (EditText) findViewById(R.id.name_field);
+        userNameInput.setText(null);
     }
 }
